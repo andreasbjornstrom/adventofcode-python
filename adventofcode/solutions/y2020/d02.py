fcce5e4cc5fdb7678e187709fe642379bbd7b890
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-02 21:51:36 +0100

    Day2 2020

--- adventofcode/solutions/y2020/d02.py
+++ adventofcode/solutions/y2020/d02.py
@@ -4,6 +4,38 @@ Run it with the command `python -m adventofcode run_solution -y 2020 2` from the
 '''
 from adventofcode.types import Solution
 
+
+def part1(data):
+    valid_passwords = 0
+
+    for string in data.splitlines():
+        interval, char_with_separator, password = string.split()
+        char = char_with_separator[0]
+        min_count, max_count = interval.split("-")
+
+        count = password.count(char)
+        if count < int(min_count) or count > int(max_count):
+            continue
+        valid_passwords += 1
+
+    return valid_passwords
+
+
+def part2(data):
+    valid_passwords = 0
+
+    for string in data.splitlines():
+        interval, char_with_separator, password = string.split()
+        char = char_with_separator[0]
+        min_count, max_count = interval.split("-")
+
+        if (password[int(min_count) - 1] == char) != \
+                (password[int(max_count) - 1] == char):
+            valid_passwords += 1
+        continue
+
+    return valid_passwords
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    return part1(data), part2(data)
--- adventofcode/tests/y2020/test_2020_02.py
+++ adventofcode/tests/y2020/test_2020_02.py
@@ -4,6 +4,12 @@ Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
 from adventofcode.solutions.y2020.d02 import run
 
+data = """1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc"""
+
+
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+    # not yet implemented!
+
+    assert run(data) == (2, 1)
