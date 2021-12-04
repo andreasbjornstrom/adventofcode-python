63f8fbc2da90a50b7ca3325a4da64fb3ec83c217
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-04 22:04:01 +0100

    Day4.. oh the struggle. Clean up needed.

--- adventofcode/solutions/y2021/d04.py
+++ adventofcode/solutions/y2021/d04.py
@@ -2,8 +2,117 @@
 Solution for day 4 of the 2021 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2021 4` from the project root.
 '''
+from functools import reduce
+
 from adventofcode.types import Solution
 
+
+def part1(data):
+    sum_of_unmarked, current_number = 0, 0
+    rows = data.splitlines()
+    drawn_numbers = rows[0].split(",")
+    boards = []
+
+    board_zero = []
+    row_counter = 0
+    for i, row in enumerate(rows):
+        if i == 0 or not row:
+            continue
+
+        board_zero.append(row.split())
+        row_counter += 1
+        if row_counter == 5:
+            boards.append(board_zero)
+            print(board_zero)
+            row_counter = 0
+            print(f"len of boards: {len(boards)}")
+            board_zero = []
+
+    sliding_winning_number = []
+    for drawn_number in drawn_numbers:
+        sliding_winning_number.append(drawn_number)
+        #       print(f"{sliding_winning_number}")
+        winning_row = []
+        for board in boards:
+            winning_row = getMatch(board, sliding_winning_number)
+            if winning_row is not None:
+                all_in_board = [row for sublist in board for row in sublist]
+                all_in_board_as_int = [int(col) for col in all_in_board]
+                drawn_as_int = [int(col) for col in sliding_winning_number]
+                only_unmarked = set(all_in_board_as_int) - set(drawn_as_int)
+                sum_of_unmarked = reduce(lambda a, b: a + b, only_unmarked)
+                current_number = int(drawn_number)
+                break
+        if winning_row is not None:
+            print(f"..")
+            break
+
+    return sum_of_unmarked * current_number
+
+
+def getMatch(board, sliding_winning_number):
+    for r in range(0, 5):
+        if set(board[r]) <= set(sliding_winning_number):
+            print(f"yey!: {board[r]}")
+            return board[r]
+    for c in range(0, 5):
+        col_values = []
+        for r in range(0, 5):
+            col_values.append(board[r][c])
+        if set(sliding_winning_number) >= set(col_values):
+            print(f"Win!: {col_values}")
+            return col_values
+    return None
+
+
+def part2(data):
+    sum_of_unmarked, current_number = 0, 0
+    rows = data.splitlines()
+    drawn_numbers = rows[0].split(",")
+    boards = []
+
+    board_zero = []
+    row_counter = 0
+    for i, row in enumerate(rows):
+        if i == 0 or not row:
+            continue
+
+        board_zero.append(row.split())
+        row_counter += 1
+        if row_counter == 5:
+            boards.append(board_zero)
+            print(board_zero)
+            row_counter = 0
+            print(f"len of boards: {len(boards)}")
+            board_zero = []
+
+    sliding_winning_number = []
+    boards_with_win = set()
+    for drawn_number in drawn_numbers:
+        sliding_winning_number.append(drawn_number)
+      #  print(f"{sliding_winning_number}")
+        winning_row = None
+        for i, board in enumerate(boards):
+            winning_row = getMatch(board, sliding_winning_number)
+            if winning_row is not None:
+                all_in_board = [row for sublist in board for row in sublist]
+                all_in_board_as_int = [int(col) for col in all_in_board]
+                drawn_as_int = [int(col) for col in sliding_winning_number]
+                only_unmarked = set(all_in_board_as_int) - set(drawn_as_int)
+                sum_of_unmarked = reduce(lambda a, b: a + b, only_unmarked)
+                current_number = int(drawn_number)
+                boards_with_win.add(i)
+                if len(boards_with_win) == len(boards):
+                    print(f"finally.. {winning_row}")
+                    break
+                winning_row = None
+        if winning_row is not None:
+            print(f"..")
+            break
+    print(f"{sum_of_unmarked} {current_number}")
+    return sum_of_unmarked * current_number
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    # not yet implemented!
+    return part1(data), part2(data)
--- adventofcode/tests/y2021/test_2021_04.py
+++ adventofcode/tests/y2021/test_2021_04.py
@@ -4,6 +4,26 @@ Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
 from adventofcode.solutions.y2021.d04 import run
 
+data ="""7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"""
+
 def test_run() -> None:
   # not yet implemented!
-  assert run('') == (None, None)
+  assert run(data) == (4512, 1924)
