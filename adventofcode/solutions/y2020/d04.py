50ef30d5cfffd2771f6693666d9a77fe4589286b
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-07 21:12:24 +0100

    a bit of a struggle, to many ifs to keep track of. not really fun.

--- adventofcode/solutions/y2020/d04.py
+++ adventofcode/solutions/y2020/d04.py
@@ -2,8 +2,79 @@
 Solution for day 4 of the 2020 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2020 4` from the project root.
 '''
+import re
+
 from adventofcode.types import Solution
 
+
+class Passport:
+    byr: str = 0  # (Birth Year)
+    iyr: str = 0  # (Issue Year)
+    eyr: str = 0  # (Expiration Year)
+    hgt: str = ""  # (Height)
+    hcl: str = ""  # (Hair Color)
+    ecl: str = ""  # (Eye Color)
+    pid: str = ""  # (Passport ID)
+    cid: str = ""  # (Country ID)
+
+    def __init__(self, passport_as_string) -> None:
+        for key_value in passport_as_string.split():
+            key, value = key_value.split(":")
+            # print(f"key: {key}, value: {value}")
+            ## reqs python 3.10..
+            if key == 'byr':
+                self.byr = value
+            elif key == 'iyr':
+                self.iyr = value
+            elif key == 'eyr':
+                self.eyr = value
+            elif key == 'hgt':
+                self.hgt = value
+            elif key == 'hcl':
+                self.hcl = value
+            elif key == 'ecl':
+                self.ecl = value
+            elif key == 'pid':
+                self.pid = value
+            elif key == 'cid':
+                self.cid = value
+            else:
+                print("MEEEP?!")
+
+    def __repr__(self) -> str:
+        return f"\n has fields: byr: {self.byr},\n {self.iyr},\n {self.eyr},\n {self.hgt},\n {self.hcl},\n {self.pid},\n {self.cid}"
+
+    def validate_part2(self) -> bool:
+        valid_ecl = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        return 1920 <= int(self.byr) <= 2002 and \
+               2010 <= int(self.iyr) <= 2020 and \
+               2020 <= int(self.eyr) <= 2030 and \
+               self.validate_hgt() and \
+               re.match('^#[0-9a-f]{6}$', self.hcl) and \
+               self.ecl in valid_ecl and \
+               re.match('^[0-9]{9}$', self.pid)
+
+    def validate_part1(self) -> bool:
+            return bool(self.byr) and \
+                   bool(self.iyr) and \
+                   bool(self.eyr) and \
+                   bool(self.hgt) and \
+                   bool(self.hcl) and \
+                   bool(self.ecl) and \
+                   bool(self.pid)
+
+    def validate_hgt(self):
+        if self.hgt.endswith("cm"):
+            height, _ = self.hgt.split("cm")
+            return 150 <= int(height) <= 193
+        elif self.hgt.endswith("in"):
+            height, _ = self.hgt.split("in")
+            return 59 <= int(height) <= 76
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    passports = [Passport(passport_lines) for passport_lines in data.split("\n\n")]
+    print(passports)
+    valid_passports1 = [passport for passport in passports if passport.validate_part1()]
+    valid_passports2 = [passport for passport in passports if passport.validate_part2()]
+    return len(valid_passports1), len(valid_passports2)
--- adventofcode/tests/y2020/test_2020_04.py
+++ adventofcode/tests/y2020/test_2020_04.py
@@ -4,6 +4,34 @@ Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
 from adventofcode.solutions.y2020.d04 import run
 
+
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+    data = """ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
+
+iyr:2019
+cid:314
+eyr:2039 hcl:#cfa07d hgt:171cm ecl:#0180ce byr:2006 pid:8204115568
+
+eyr:2026
+hcl:#733820
+pid:230583200 byr:1997
+ecl:brn
+iyr:2010
+hgt:179cm"""
+
+    assert run(data) == (4, 3)
+
+if __name__ == '__main__':
+    test_run()
