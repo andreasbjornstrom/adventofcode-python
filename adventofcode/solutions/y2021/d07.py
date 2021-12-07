1638cc2966ab80ced79e201b0992a6fa97874151
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-07 11:39:15 +0100

    Day 7, quite easy

--- adventofcode/solutions/y2021/d07.py
+++ adventofcode/solutions/y2021/d07.py
@@ -4,6 +4,44 @@ Run it with the command `python -m adventofcode run_solution -y 2021 7` from the
 '''
 from adventofcode.types import Solution
 
+
+def part1(data):
+    positions = [int(pos) for pos in data.split(",")]
+
+    print(
+        f"positions: {positions}, min: {min(positions)}, {max(positions)},"
+        f" first try: {(max(positions) - min(positions)) / 2}")
+
+    fuel_cost = None
+    for goal in range(min(positions), max(positions)):
+        _fuel_cost = sum([abs((pos - goal)) for pos in positions])
+        print(f"trying out {goal}, resulted in {_fuel_cost}")
+        if fuel_cost is None or _fuel_cost < fuel_cost:
+            fuel_cost = _fuel_cost
+        else:
+            return fuel_cost
+
+
+def triangular_number(n):
+    return sum(range(n + 1))
+
+
+def part2(data):
+    positions = [int(pos) for pos in data.split(",")]
+
+    print(
+        *
+        f"positions: {positions}, min: {min(positions)}, {max(positions)}")
+
+    fuel_cost = None
+    for target in range(min(positions), max(positions)):
+        _fuel_cost = sum([triangular_number(abs((pos - target))) for pos in positions])
+        print(f"trying out {target}, resulted in {_fuel_cost}")
+        if fuel_cost is None or _fuel_cost < fuel_cost:
+            fuel_cost = _fuel_cost
+        else:
+            return fuel_cost
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    return part1(data), part2(data)
--- adventofcode/tests/y2021/test_2021_07.py
+++ adventofcode/tests/y2021/test_2021_07.py
@@ -2,8 +2,11 @@
 Test for year 2021, day 7 solution.
 Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
-from adventofcode.solutions.y2021.d07 import run
+from adventofcode.solutions.y2021.d07 import run, triangular_number
+
 
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+    assert triangular_number(1) == 1
+    assert triangular_number(2) == 3
+    assert triangular_number(3) == 6
+    assert run('16,1,2,0,4,2,7,1,2,14') == (37, 168)
