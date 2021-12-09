f6151a4a2d53126ea1f2f0bea098e7057f1525b7
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-09 19:17:52 +0100

    Day 8.

--- adventofcode/solutions/y2021/d08.py
+++ adventofcode/solutions/y2021/d08.py
@@ -4,6 +4,98 @@ Run it with the command `python -m adventofcode run_solution -y 2021 8` from the
 '''
 from adventofcode.types import Solution
 
+one = 2
+four = 4
+seven = 3
+eight = 7
+two_three_five = 5
+zero_six_nine = 6
+
+
+def get_zero_six_nine(digit_set, chars_in_one, chars_in_four, chars_in_seven):
+    if chars_in_one:
+        if not chars_in_one.issubset(digit_set):
+            return "6"
+    if chars_in_four:
+        if chars_in_four.issubset(digit_set):
+            return "9"
+    if chars_in_seven:
+        if not chars_in_seven.issubset(digit_set):
+            return "6"
+    return "0"
+
+
+def get_two_three_five(digit_set, one1, four_chars, seven_chars):
+    if one1 and one1.issubset(digit_set):
+        return "3"
+    if seven_chars and seven_chars.issubset(digit_set):
+        return "3"
+    if four_chars and one1:
+        diff = four_chars - one1
+        if diff.issubset(digit_set):
+            return "5"
+    return "2"
+
+
+def part2(data):
+    extra_data = [row.split("|")[0].split() for row in data.splitlines()]
+    four_digits_encoded = [row.split("|")[1].split() for row in data.splitlines()]
+    total = 0
+    for i, output_values in enumerate(four_digits_encoded):
+
+        all_data = output_values + extra_data[i]
+        all_data.sort(key=lambda x: len(x), reverse=True)
+        chars_in_one = set()
+        chars_in_four = set()
+        chars_in_seven = set()
+        for digit in all_data:
+            letters_for_digit = set(digit)
+            print(f"working with {len(digit)} {digit}")
+
+            length = len(digit)
+            if length == one:
+                chars_in_one = letters_for_digit
+            elif length == seven:
+                chars_in_seven = letters_for_digit
+            elif length == four:
+                chars_in_four = letters_for_digit
+
+        row_sum = ""
+        for digit in output_values:
+            length = len(digit)
+            digit_set = set(digit)
+            if length == one:
+                row_sum += "1"
+                continue
+            elif length == four:
+                row_sum += "4"
+                continue
+            elif length == seven:
+                row_sum += "7"
+                continue
+            elif length == eight:
+                row_sum += "8"
+                continue
+
+            if length == two_three_five:
+                row_sum += get_two_three_five(digit_set, chars_in_one, chars_in_four, chars_in_seven)
+            elif length == zero_six_nine:
+                row_sum += get_zero_six_nine(digit_set, chars_in_one, chars_in_four, chars_in_seven)
+        total += int(row_sum)
+
+    return total
+
+
+def part1(data):
+    valid_numbers = [one, four, seven, eight]
+    four_digits_encoded = [row.split("|")[1].split() for row in data.splitlines()]
+    flatten = [digit for row in four_digits_encoded for digit in row]
+    count = 0
+    for digit in flatten:
+        if len(digit) in valid_numbers:
+            count += 1
+    return count
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    return (part1(data)), part2(data)
--- adventofcode/tests/y2021/test_2021_08.py
+++ adventofcode/tests/y2021/test_2021_08.py
@@ -4,6 +4,18 @@ Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
 from adventofcode.solutions.y2021.d08 import run
 
+data = """be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"""
+
+
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+    # not yet implemented!
+    assert run(data) == (26, 61229)
