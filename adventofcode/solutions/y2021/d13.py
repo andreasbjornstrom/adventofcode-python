bae1b58a0c71503c5a0443dacd55fdf04c0ab43d
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-14 22:59:47 +0100

    fun day, nice twist on part 2

--- adventofcode/solutions/y2021/d13.py
+++ adventofcode/solutions/y2021/d13.py
@@ -2,8 +2,47 @@
 Solution for day 13 of the 2021 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2021 13` from the project root.
 '''
+import numpy as np
+
 from adventofcode.types import Solution
 
+
+def part1(data, part1=False):
+    rows = [row for row in data.splitlines() if row and 'fold' not in row]
+    board = np.zeros(
+        (max([int(row.split(",")[1]) for row in rows]) + 1,
+         max([int(row.split(",")[0]) for row in rows]) + 1))
+    for row in data.splitlines():
+        if not row:
+            continue
+        if 'fold along x=' in row:
+            # reverse needed
+            x = int(row.split("=")[1])
+            base = board[:, 0:x]
+            fold = board[:, x + 1:]
+            reversed = np.flip(fold[::-1])
+            print(base, "\n", reversed, "\n")
+            board = base + reversed
+            print(board)
+            if part1:
+                return board
+            continue
+        if 'fold along y=' in row:
+            print("folding..")
+            y = int(row.split("=")[1])
+            base = board[0:y, :]
+            fold = board[y + 1:, :]
+            reversed = np.fliplr(np.flip(fold))
+            print(base, "\n", reversed, "\n", fold)
+            board = base + reversed
+            print(board)
+            if part1:
+                return board
+            continue
+        y, x = [int(c) for c in row.split(",")]
+        board[x][y] = 1
+    return board
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    return np.count_nonzero(part1(data, part1=True)), part1(data, part1=False)
--- adventofcode/tests/y2021/test_2021_13.py
+++ adventofcode/tests/y2021/test_2021_13.py
@@ -2,8 +2,40 @@
 Test for year 2021, day 13 solution.
 Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
+import numpy as np
+
 from adventofcode.solutions.y2021.d13 import run
 
+data = """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"""
+
+
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+    part1, part2 = run(data)
+    assert np.array_equal(part2, np.array([[1., 1., 1., 1., 1.],
+                                           [1., 0., 0., 0., 1.],
+                                           [1., 0., 0., 0., 1.],
+                                           [1., 0., 0., 0., 1.],
+                                           [1., 2., 2., 1., 1.],
+                                           [0., 0., 0., 0., 0.],
+                                           [0., 0., 0., 0., 0.]]))
+    assert part1 == 17
