df4ca00639e8900b55afc8b38268c5a50857f783
Andreas Bjornstrom <andreas.bjornstrom@r2m.se>
2021-12-20 22:23:29 +0100

    Got stuck at day2, rewrote everything a few times.

--- adventofcode/solutions/y2021/d14.py
+++ adventofcode/solutions/y2021/d14.py
@@ -2,8 +2,67 @@
 Solution for day 14 of the 2021 Advent of Code calendar.
 Run it with the command `python -m adventofcode run_solution -y 2021 14` from the project root.
 '''
+
 from adventofcode.types import Solution
 
+
+def count_chars(rules, template, steps):
+    # Template:     NNCB NN NC CB
+    # After step 1: NCNBCHB
+    # After step 2: NBCCNBBBCBHCB
+    # After step 3: NBBBCNCCNBBNBNBBCHBHHBCHB
+    # After step 4: NBBNBNBBCCNBCNCCNBBNBBNBBBNBBNBBCBHCBHHNHCBBCBHCB
+    pairs = {}
+    first_char, last_char = template[0], template[-1]
+    for i, char in enumerate(template):
+        if i == 0:
+            continue
+        if template[i - 1] + char in pairs:
+            pairs[template[i - 1] + char] += 1
+        else:
+            pairs[template[i - 1] + char] = 1
+
+
+    print(rules)
+
+    for _ in range(steps):
+        new_pairs = {}
+        for pair, current_count in pairs.items():
+            p1, p2 = rules[pair]
+
+            if p1 in new_pairs:
+                new_pairs[p1] += current_count
+            else:
+                new_pairs[p1] = current_count
+
+            if p2 in new_pairs:
+                new_pairs[p2] += current_count
+            else:
+                new_pairs[p2] = current_count
+        pairs = new_pairs
+    #        print(pairs)
+
+    chars = {}
+    for pair in pairs:
+        for c in pair:
+            if c in chars:
+                chars[c] += pairs[pair]
+            else:
+                chars[c] = pairs[pair]
+    chars[first_char] += 1
+    chars[last_char] += 1
+    print(chars)
+    return (max(chars.values()) - min(chars.values())) / 2
+
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    rows = data.splitlines()
+    rules2 = {}
+    for row in rows[1:]:
+        if "->" not in row:
+            continue
+        pair, new_char = row.split(" -> ", 2)
+        rules2[pair] = (pair[0] + new_char, new_char + pair[1])
+    part1 = count_chars(rules2, rows[0], 10)
+    part2 = count_chars(rules2, rows[0], 40)
+    return part1, part2
--- adventofcode/tests/y2021/test_2021_14.py
+++ adventofcode/tests/y2021/test_2021_14.py
@@ -4,6 +4,26 @@ Run tests from project root with `PYTHONPATH=$(pwd) py.test`.
 '''
 from adventofcode.solutions.y2021.d14 import run
 
+data = """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C
+"""
+
+
 def test_run() -> None:
-  # not yet implemented!
-  assert run('') == (None, None)
+    assert run(data) == (1588, 2188189693529)
