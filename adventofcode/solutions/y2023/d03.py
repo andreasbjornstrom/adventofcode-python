6064a5369c829014423970ead3734775d0a973a0
Andreas Björnström <andreas.bjornstrom@carsmart.se>
2023-12-12 22:50:54 +0100

    Some more..

--- adventofcode/solutions/y2023/d03.py
+++ adventofcode/solutions/y2023/d03.py
@@ -15,70 +15,38 @@ dirs = [(-1, -1), (0, -1), (1, -1),
         (-1, 1), (0, 1), (1, 1)]
 
 
-def part2(board):
-    current_number = ""
-    start_matches_count = 0
-    star_position = "star"
-    stars = {}
-
-    for y, row in enumerate(board):
-        for x, cell in enumerate(row):
-            if not is_number(cell):
-                if start_matches_count == 1 and current_number != "":
-                    stars.setdefault(star_position, []).append(
-                        int(current_number))
-                    print(star_position, int(current_number), stars)
-                current_number = ""
-                start_matches_count = 0
-                star_position = None
-            else:
-                current_number += cell
-                for direction in dirs:
-                    pos = get_cell_data(
-                        y, x, direction[0], direction[1], board)
-                    if is_star(pos) and star_position != f"{y + direction[0]},{x + direction[1]}":
-                        print("At cell", cell, " pos: ",
-                              f"{y + direction[0]},{x + direction[1]}")
-                        start_matches_count += 1
-                        star_position = f"{y + direction[0]},{x + direction[1]}"
-
-    if start_matches_count == 1 and current_number != "":
-        stars[star_position] = stars[star_position] * int(current_number)
-
-    return sum(star[0] * star[1] for star in stars.values() if len(star) > 1)
-
-
 def _part2(board: [[]]) -> int:
     current_number = ""
-    start_matches_counter = 0
-    star_pos = "star"
+    star_matches_counter = 0
+    coordinates_for_star = "star"
     stars = {}
 
     for y, row in enumerate(board):
         for x, cell in enumerate(row):
             if not is_number(cell):
-                if start_matches_counter == 1 and current_number != "":
-                    if star_pos not in stars.keys():
-                        stars[star_pos] = [int(current_number)]
-                        print(star_pos, int(current_number), stars)
+                if star_matches_counter == 1 and current_number != "":
+                    if coordinates_for_star not in stars.keys():
+                        stars[coordinates_for_star] = [int(current_number)]
+                        print(coordinates_for_star, int(current_number), stars)
                     else:
-                        stars[star_pos].append(int(current_number))
-                        print(star_pos, int(current_number), stars)
+                        stars[coordinates_for_star].append(int(current_number))
+                        print(coordinates_for_star, int(current_number), stars)
                 current_number = ""
-                start_matches_counter = 0
-                star_pos = None
+                star_matches_counter = 0
+                coordinates_for_star = None
             else:
                 current_number += cell
                 for direction in dirs:
                     pos = get_cell_data(y, x, direction[0], direction[1], board)
-                    if is_star(pos) and star_pos != f"{y + direction[0]},{x + direction[1]}":
+                    coordinates_for_pos = f"{y + direction[0]},{x + direction[1]}"
+                    if is_star(pos) and coordinates_for_star != coordinates_for_pos:
                         print("At cell: ", cell, " pos: ",
-                              f"{y + direction[0]},{x + direction[1]}")
-                        start_matches_counter += 1
-                        star_pos = f"{y + direction[0]},{x + direction[1]}"
+                              coordinates_for_pos)
+                        star_matches_counter += 1
+                        coordinates_for_star = coordinates_for_pos
 
-    if start_matches_counter == 1 and current_number != "":
-        stars[star_pos] = stars[star_pos] * int(current_number)
+    if star_matches_counter == 1 and current_number != "":
+        stars[coordinates_for_star] = stars[coordinates_for_star] * int(current_number)
 
     result = 0
     for star in stars.values():
--- adventofcode/solutions/y2023/d04.py
+++ adventofcode/solutions/y2023/d04.py
@@ -4,6 +4,26 @@ Run it with the command `python -m adventofcode run_solution -y 2023 4` from the
 '''
 from adventofcode.types import Solution
 
+
 def run(data: str) -> Solution:
-  # not yet implemented!
-  return (None, None)
+    result = 0
+    for row in data.splitlines():
+        (card, numbers) = row.split(": ")
+        (winning, given) = numbers.split(" | ")
+        winning_numbers = [int(item) for item in winning.split()]
+        given_numbers = [int(item) for item in given.split()]
+        count_matches = 0
+        count_score = 0
+        for number in given_numbers:
+            if number in winning_numbers:
+                count_matches += 1
+                if count_matches == 1:
+                    count_score = 1
+                else:
+                    count_score = count_score * 2
+                print(f"{number} is a match")
+        if count_matches > 0:
+            result += count_score
+            print(f"Card {card} has {count_matches} matches, current score: {result}")
+
+    return result, None
